b3b1206bc24bdf9dea197a846927b3eacbae01b7
guilhermetell <guilhermetell09@gmail.com>
2016-10-07 16:16:55 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title>Inserir Produto</title>
+     <meta charset="utf-8">
+     <link rel="stylesheet" href="estilo.css">
+     <script type="text/javascript" href=javascript.js></script>
+</head>
+<body>
+<div id="interface">
+     <form action="" method="POST">
+
+          <fieldset>
+               <legend>Input texts</legend>
+
+               <div class="formulario">
+                    <p>
+                         <input name="user" type="text" >
+                    </p>
+                    <p>
+                         <label class="lab">usuario</label>
+                    </p>
+               </div>
+
+          </fieldset>
+
+          <fieldset>
+               <legend>Botões</legend>
+               <label>Select</label>
+               <select name="pais">
+                    <option value="brasil"> Brasil </option>
+                    <option value="noruega">Noruega</option>
+                    <option value="holanda">Holanda</option>
+               </select>
+               <label>checkboxes</label>
+               <input type="checkbox" value="valor">
+          </fieldset>
+
+     </form>
+     <div>
+          <div id="esquerda"></div>
+          <div id="direita"></div>
+     </div>
+
+</div>
+</body>
+</html>
+<?php
+
+
+
+
+
+ ?>
---
+++ javascript.js
@@ -0,0 +1,8 @@
+function habilitar(){
+     //Especificações GSM:
+     if(document.getElementById('on')){
+          document.getElementById('on').id="off";
+     } else {
+          document.getElementById('off').id="on");
+     }
+}
